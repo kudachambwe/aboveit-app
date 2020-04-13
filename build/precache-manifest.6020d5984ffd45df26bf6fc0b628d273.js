88d40c76fb3bc73369201a81c69ad1f4e5f2a3c9
Kudakwashe Chambwe <kudakwashe.chambwe@bouvet.no>
2020-04-13 21:32:59 +0200

    added zeit now cd using github actions

---
+++ build/precache-manifest.6020d5984ffd45df26bf6fc0b628d273.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3495938c4840b8d23b12809f18ae43dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b59149e7ff5a8ae9882",
+    "url": "/static/css/main.5788f03a.chunk.css"
+  },
+  {
+    "revision": "6856a3ae0475ca129235",
+    "url": "/static/js/2.315f1ac8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.315f1ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b59149e7ff5a8ae9882",
+    "url": "/static/js/main.70345b19.chunk.js"
+  },
+  {
+    "revision": "7034cf0aa9bf7a02290f",
+    "url": "/static/js/runtime-main.24266cb6.js"
+  }
+]);
